d1b7ba3e5f82e7bceede938cf6c466f65c282398
salahphd0 <87438246+salahphd0@users.noreply.github.com>
2021-08-18 09:29:01 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ibrahim-firebase',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
